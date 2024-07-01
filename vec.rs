75bc757a47f9b25b2df0c7d9500840191adb3003
Elijah Mooring <vehmloewff@gmail.com>
2024-07-01 06:22:50 -0700

    feat!: remove `ConsumedVec` in favor of Vec::drain

--- vec.rs
+++ vec.rs
@@ -1,30 +1,3 @@
-pub struct ConsumedVec<'a, T>(&'a mut Vec<T>);
-
-impl<'a, T> ConsumedVec<'_, T> {
-    pub fn new(vec: &mut Vec<T>) -> ConsumedVec<'_, T> {
-        vec.reverse();
-        ConsumedVec(vec)
-    }
-}
-
-impl<T> Iterator for ConsumedVec<'_, T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.pop()
-    }
-}
-
-pub trait ConsumableVec<T> {
-    fn consume(&mut self) -> ConsumedVec<'_, T>;
-}
-
-impl<T> ConsumableVec<T> for Vec<T> {
-    fn consume(&mut self) -> ConsumedVec<'_, T> {
-        ConsumedVec::new(self)
-    }
-}
-
 pub fn nest_vector<T>(vec: Vec<T>, every_n: usize) -> Vec<Vec<T>> {
     let mut nested_vec = Vec::new();
     let mut counter = 1;
